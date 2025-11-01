262415e0fb8fc50c73194662d8afaeca589647b9
dev-suryajith <suryajithss08@gmail.com>
2025-11-01 17:02:28 +0530

    Admin Dashboard v2.0.2

--- src/services/allAPI.js
+++ src/services/allAPI.js
@@ -1,46 +1,46 @@
-import commonAPI from "./commonAPI"
+import adminAPI from "./adminAPI"
 import BASEURL from "./serverURL"
 
 // ----------------- Doctor -----------------
 export const addDoctorApi = async (data) => {
-    return await commonAPI("POST", `${BASEURL}/doctors`, data)
+    return await adminAPI("POST", `${BASEURL}/doctors`, data)
 }
 
 export const getAllDoctorAPI = async () => {
-    return await commonAPI("GET", `${BASEURL}/doctors`, {})
+    return await adminAPI("GET", `${BASEURL}/doctors`, {})
 }
 
 export const deleteDoctorApi = async (id) => {
-    return await commonAPI("DELETE", `${BASEURL}/doctors/${id}`)
+    return await adminAPI("DELETE", `${BASEURL}/doctors/${id}`)
 }
 
 export const updateDoctorApi = async (id, doc) => {
-    return await commonAPI('PUT', `${BASEURL}/doctors/${id}`, doc)
+    return await adminAPI('PUT', `${BASEURL}/doctors/${id}`, doc)
 }
 
 
 // ----------------- Patient -----------------
 
 export const getAllPatientAPI = async () => {
-    return await commonAPI("GET", `${BASEURL}/patients`, {})
+    return await adminAPI("GET", `${BASEURL}/patients`, {})
 }
 
 export const updatePatientApi = async (id, data) => {
-    return await commonAPI('PUT', `${BASEURL}/patients/${id}`, data)
+    return await adminAPI('PUT', `${BASEURL}/patients/${id}`, data)
 }
 
 export const deletePatientAPI = async (id) => {
-    return await commonAPI("DELETE", `${BASEURL}/doctors/${id}`)
+    return await adminAPI("DELETE", `${BASEURL}/doctors/${id}`)
 }
 
 
 // ----------------- Appointment -----------------
 
 export const getAllAppointmentAPI = () =>
-  commonAPI("GET", `${BASEURL}/appointments`, "");
+  adminAPI("GET", `${BASEURL}/appointments`, "");
 
 export const deleteAppointmentAPI = (id) =>
-  commonAPI("DELETE", `${BASEURL}/appointments/${id}`, "");
+  adminAPI("DELETE", `${BASEURL}/appointments/${id}`, "");
 
 export const updateAppointmentApi = (id, data) =>
-  commonAPI("PUT", `${BASEURL}/appointments/${id}`, data);
+  adminAPI("PUT", `${BASEURL}/appointments/${id}`, data);
--- src/services/commonAPI.js
+++ src/services/commonAPI.js
@@ -1,15 +1,31 @@
-import axios from "axios"
+import axios from "axios";
 
-const commonAPI = async (httpMethod, url, reqBody) => {
-    const reqConfig = {
-        method: httpMethod,
-        url,
-        data: reqBody
+const commonAPI = async (method, url, data = {}) => {
+  try {
+    const response = await axios({ 
+      method, 
+      url, 
+      data,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    return response.data;
+  } catch (error) {
+    console.error('API Error:', {
+      url,
+      method,
+      status: error.response?.status,
+      statusText: error.response?.statusText,
+      message: error.message,
+      data: error.response?.data
+    });
+    // Return empty array for GET requests on error, null for others
+    if (method === 'GET') {
+      return [];
     }
-    return await axios(reqConfig).then(res=>{
-        return res
-    }).catch(err=>{
-        return err
-    })
-}
-export default commonAPI
+    return null;
+  }
+};
+
+export default commonAPI;
